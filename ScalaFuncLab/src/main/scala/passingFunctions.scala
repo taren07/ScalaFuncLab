927c744661c760264125fbc641e4110632a151a3
taren07 <taro.enchi@gmail.com>
2024-04-15 22:31:11 +0900

    add pcaticing map

--- ScalaFuncLab/src/main/scala/passingFunctions.scala
+++ ScalaFuncLab/src/main/scala/passingFunctions.scala
@@ -12,11 +12,32 @@ object passingFunctions extends App {
     println(words.sortBy(score))
     assert(words.sortBy(score) == List("java", "scala", "haskell"))
   }
+
+  // sortBy
   {
     def score(word: String): Int = word.replaceAll("a", "").length
     val words = List("scala", "java", "haskell")
     val sortedWords = words.sortBy(score)
     assert(sortedWords == List("java", "scala", "haskell"))
     assert(List("java", "scala", "haskell").sortBy(score) == sortedWords)
+  }
+
+  // Practicing map
+  {
+
+    def numberOfS(s: String): Int = s.length - s.replaceAll("s", "").length
+    val numberOfSs = List("scala", "java", "haskell").map(numberOfS)
+    println(numberOfSs)
+    assert(numberOfSs == List(1, 0, 1))
+
+    def negative(i: Int): Int = -i
+    val negatives = List(5, 1, 2, 4, 0).map(negative)
+    assert(negatives == List(-5, -1, -2, -4, 0))
+    println(negatives)
+
+    def double(i: Int): Int = i * 2
+    val doubles = List(5, 1, 2, 4, 0).map(double)
+    assert(doubles == List(10, 2, 4, 8, 0))
+    println(doubles)
   }
 }
