117e1740107eb650381bb44e316409e9f21b1d33
taren07 <taro.enchi@gmail.com>
2024-04-18 23:34:57 +0900

    add case class: programmingLanguage

---
+++ ScalaFuncLab/src/main/scala/programmingLanguages.scala
@@ -0,0 +1,6 @@
+object programmingLanguages extends App {
+    case class programmingLanguage(name: String, year: Int)
+    val javalang = programmingLanguage("Java", 1995)
+    val scalalang = programmingLanguage("Scala", 2004)
+    
+}
