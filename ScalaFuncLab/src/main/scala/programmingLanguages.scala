1929a8a3326c4cb56cdc7598d0d3a8ea83a45917
taren07 <taro.enchi@gmail.com>
2024-04-23 22:36:08 +0900

    add programmingLanguage class

--- ScalaFuncLab/src/main/scala/programmingLanguages.scala
+++ ScalaFuncLab/src/main/scala/programmingLanguages.scala
@@ -1,6 +1,14 @@
 object programmingLanguages extends App {
+
     case class programmingLanguage(name: String, year: Int)
+
     val javalang = programmingLanguage("Java", 1995)
     val scalalang = programmingLanguage("Scala", 2004)
-    
+    assert(javalang.name == "Java")
+    assert(scalalang.name == "Scala")
+    assert(scalalang.name.length == 5)
+    assert((scalalang.year + javalang.year) / 2 == 1999)
+
+    val languages = List(javalang, scalalang)
+    assert(languages == List(ProgrammingLanguage("Java", 1995), ProgrammingLanguage("Scala", 2004)))
 }
