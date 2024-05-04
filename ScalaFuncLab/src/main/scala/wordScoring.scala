b8495361228cf2aca1da064f2dd4ea0feace7ec6
taren07 <taro.enchi@gmail.com>
2024-05-04 23:24:01 +0900

    add wordScoring object

---
+++ ScalaFuncLab/src/main/scala/wordScoring.scala
@@ -0,0 +1,56 @@
+object wordScoring extends App {
+
+    def score(word: String): Int = word.replaceAll("a", "").length
+
+    {
+        assert(List("rust", "java").sortBy(score) == List("java", "rust"))
+    }
+
+    val words = List("scala", "java", "haskell", "golang", "rust")
+
+    {
+        val wordRanking = words.sortBy(score).reverse
+        assert(wordRanking == List("java", "scala", "haskell", "golang", "rust"))
+    }
+
+    {
+        def rankedWords(wordScore: String => Int, words: List[String]): List[String] = {
+            def negativeScore(word: String): Int = -wordScore(word)
+            words.sortBy(negativeScore)
+        }
+        assert(rankedWords(score, List("rust", "java")) == List("rust", "java"))
+
+        val wordRanking = rankedWords(score, words)
+        assert(wordRanking == List("haskell", "rust", "scala", "java", "ada"))
+    }
+
+    {
+        def rankedWords(wordScore: String => Int, words: List[String]): List[String] = {
+            words.sortBy(wordScore).reverse
+        }
+
+        assert(rankedWords(score, List("rust", "java")) == List("java", "rust"))
+
+        {
+            val wordRanking = rankedWords(score, words)
+            assert(wordRanking == List("java", "scala", "haskell", "golang", "rust"))
+        }
+
+        def scoreWithBonus(word: String): Int = {
+            val base = score(word)
+            if (word.contains("c")) base + 5 else base
+        }
+
+        {
+            val wordRanking = rankedWords(scoreWithBonus, words)
+            assert(wordRanking == List("java", "scala", "haskell", "golang", "rust"))
+        }
+
+        def bonus(word: String): Int = if (word.contains("c")) 5 else 0
+
+        {
+            val wordRanking = rankedWords(w => score(w) + bonus(w), words)
+            assert(wordRanking == List("java", "scala", "haskell", "golang", "rust"))
+        }
+    }
+}
