b4c1843a96d988b27da188b5945cca5da8eabee8
taren07 <taro.enchi@gmail.com>
2024-05-12 17:41:56 +0900

    create bookFriendRecommendations object

---
+++ ScalaFuncLab/src/main/scala/ch5/bookFriendRecommendations.scala
@@ -0,0 +1,22 @@
+object bookFriendRecommendations extends App {
+    case class Book(title: String, authors: List[String])
+
+    def recommendedBooks(friend: String): List[Book] = {
+        val scala = List(Book("FP in Scala", List("Chiusano", "Bjarnason")), Book("Get Programming with Scala", List("Sfregola")))
+
+        val fiction = List(Book("Harry Potter", List("Rowling")), Book("The Lord of the Rings", List("Tolkien")))
+        
+        if (friend == "Alice") scala
+        else if (friend == "Bob") fiction
+        else List.empty
+    }
+
+    val friends = List("Alice", "Bob", "Charlie")
+    val friendsBooks = friends.map(recommendedBooks)
+
+    assert(friendsBooks == List(
+        List(Book("FP in Scala", List("Chiusano", "Bjarnason")), Book("Get Programming with Scala", List("Sfregola"))),
+        List(Book("Harry Potter", List("Rowling")), Book("The Lord of the Rings", List("Tolkien"))),
+        List.empty
+    ))
+}
