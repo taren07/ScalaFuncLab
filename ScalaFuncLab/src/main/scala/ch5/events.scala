6ea6273617c3a91b865b9ddb567e50a0895c1c1c
taren07 <taro.enchi@gmail.com>
2024-05-13 23:55:42 +0900

    update

--- ScalaFuncLab/src/main/scala/ch5/events.scala
+++ ScalaFuncLab/src/main/scala/ch5/events.scala
@@ -12,6 +12,33 @@ object events extends App {
         assert(parseAdHoc("Event", 1949, 1945) == null)
     }
 
+    {
+        def parse(name: String, start: Int, end: Int): Option[Event] = {
+            if (name.size > 0 && end < 3000 & start <= end) Some(Event(name, start, end))
+            else None
+        }
+
+        assert(parse("Apollo Program", 1961, 1972) == Some(Event("Apollo Program", 1961, 1972)))
+        assert(parse("", 1939, 1945) == None)
+        assert(parse("Event", 1949, 1945) == None)
+    }
+
+    {
+        def validateName(name: String): Option[String] = if (name.size > 0) Some(name) else None
+        def validateEnd(end: Int): Option[Int] = if (end < 3000) Some(end) else None
+        def validateStart(start: Int, end: Int): Option[Int] = if (start <= end) Some(start) else None
+
+        def parse(name: String, start: Int, end: Int): Option[Event] = for {
+            validName  <- validateName(name)
+            validEnd   <- validateEnd(end)
+            validStart <- validateStart(start, end)
+        } yield Event(validName, validStart, validEnd)
+
+        assert(parse("Apollo Program", 1961, 1972) == Some(Event("Apollo Program", 1961, 1972)))
+        assert(parse("", 1939, 1945) == None)
+        assert(parse("Event", 1949, 1945) == None)
+    }
+
 
 
 }
